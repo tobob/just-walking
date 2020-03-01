7faa33eb888b5834c72e34a54990bdc5b1827c1f
Wojciech Bozek <bozek.wojciech@gmail.com>
2020-03-01 18:38:50 +0100

    Fix header

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,10 +5,17 @@ import _ from 'lodash';
 
 const Hero = () => {
   const [opacity, setOpacity] = useState(1)
+  const [top, setTop] = useState(0);
   const handleScroll = (e) => {
-    const top = e.target.scrollTop;
-    if (top < 101) {
-      setOpacity(1 - (top / 200));
+    const newTop = e.target.scrollTop;
+    if (newTop < 101) {
+      setOpacity(1 - (newTop / 200));
+    }
+    if (newTop > 40) {
+      setTop(newTop - 40)
+    }
+    if (newTop <= 40) {
+      setTop(0)
     }
   };
 
@@ -37,7 +44,7 @@ const Hero = () => {
   const { url } = data.background;
   const url2 = data.main.url;
   return <div className="hero-image">
-    <div className="hero-image-background" style={{ backgroundImage: `url(${url})`, opacity }}></div>
+    <div className="hero-image-background" style={{ backgroundImage: `url(${url})`, opacity, top: `${top}px` }}></div>
     <div className="hero-image-first" style={{ backgroundImage: `url(${url2})` }}></div>
 
     <div className='container'>
