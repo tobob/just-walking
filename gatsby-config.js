87f4142ae2e23f8f44baff1edd3589d05eda5956
Wojciech Bozek <bozek.wojciech@gmail.com>
2020-03-04 20:52:14 +0100

    Increase limit of cloudinary

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         apiKey: process.env.CLOUDINARY_API_KEY,
         apiSecret: process.env.CLOUDINARY_API_SECRET,
         resourceType: `image`,
-        maxResults: 1000,
+        maxResults: 500,
       },
     },
     {
