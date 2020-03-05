5c111da199a5ab5597ae69e1be505c9a52ca5b18
Wojciech Bozek <bozek.wojciech@gmail.com>
2020-03-05 22:34:00 +0100

    Sorting

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
 import cloudinary from "cloudinary-core"
+import Isotope from "isotope-layout/js/isotope";
 
 import "../assets/stylesheets/application.sass"
 
@@ -13,41 +14,60 @@ cl.config("cloud_name", "just-walking-me")
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
+  const iso = useRef()
+
+  useEffect(() => {
+    iso.current = new Isotope(`.posts`, {
+      itemSelector: `.single-post`,
+      layoutMode: "masonry",
+      masonry: {
+        columnWidth: 330,
+        fitWidth: true,
+      },
+      getSortData: {
+        creationdate: '[date-creation]', // value of attribute
+      },
+      // sortBy: 'creationdate',
+      sortAscending: false,
+    });
+  }, [])
 
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
+      <div className="posts-wrapper">
 
-      <div className="posts">
-        {posts.map(({ node }) => {
-          const fileName = node.fields.slug.toString().replace(/\//g, "")
-          const url = cl.url(`${fileName}.jpg`, {
-            width: 330,
-            crop: "fill",
-            height: 440,
-            secure: true,
-            effect: "saturation:40",
-          })
-          const title = node.frontmatter.title || node.fields.slug
-          const { mnpm } = node.frontmatter
-          return (
-            <Link style={{ boxShadow: `none` }} to={node.fields.slug}>
-              <div key={node.fields.slug} className="post">
-                <div className="post__header">
-                  <small>{node.frontmatter.date}</small>
+        <div className="posts">
+          {posts.map(({ node }) => {
+            const fileName = node.fields.slug.toString().replace(/\//g, "")
+            const url = cl.url(`${fileName}.jpg`, {
+              width: 330,
+              crop: "fill",
+              height: 440,
+              secure: true,
+              effect: "saturation:40",
+            })
+            const title = node.frontmatter.title || node.fields.slug
+            const { mnpm, creationDate, date } = node.frontmatter
+            return (
+              <Link className="single-post" date-creation={creationDate} style={{ boxShadow: `none` }} to={node.fields.slug}>
+                <div key={node.fields.slug} className="post" >
+                  <div className="post__header">
+                    <small>{node.frontmatter.date}</small>
+                  </div>
+                  <img src={url} />
+                  <div className="post__overlay" />
+                  <div className="post__title">
+                    <span className="post__title-main">{title}</span>
+                    {mnpm && (
+                      <span className="post__title-mnpm">{mnpm} mnpm</span>
+                    )}
+                  </div>
                 </div>
-                <img src={url} />
-                <div className="post__overlay" />
-                <div className="post__title">
-                  <span className="post__title-main">{title}</span>
-                  {mnpm && (
-                    <span className="post__title-mnpm">{mnpm} mnpm</span>
-                  )}
-                </div>
-              </div>
-            </Link>
-          )
-        })}
+              </Link>
+            )
+          })}
+        </div>
       </div>
     </Layout>
   )
@@ -71,6 +91,7 @@ export const pageQuery = graphql`
           }
           frontmatter {
             date(formatString: "MMMM DD, YYYY")
+            creationDate(formatString: "YYYYMMDD")
             title
             type
             mnpm
